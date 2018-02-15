d0bade7f5e75dc4c23bdb68aea1d61594eea8fe9
Unknown <rfcai@uw.edu>
2018-02-14 22:41:46 -0800

    2/14 update

--- ClassExercise6_RuofanCai.m
+++ ClassExercise6_RuofanCai.m
@@ -1,5 +1,6 @@
 %% 6.1
-n=101
+m=100
+n=round(100/.618);
 [X,Y] = meshgrid(linspace(-1,1,n));
 X(30:40, 10:90)=.25
 X(60:70, 10:90)=.75
@@ -12,7 +13,7 @@ figure(3)
 colormap(summer)
 
 %% 6.2
-n=101
+n=100
 [X,Y] = meshgrid(linspace(-1,1,n);
 
 sqrt(X.^2+Y.^2)
--- SineInAperture.m
+++ SineInAperture.m
@@ -69,3 +69,47 @@ subplot (1,2,2)
 image(kid10+1);colormap(gray(14))
 
 
+%%
+clear all; close all
+
+fram.apSize=100;
+fram.backLum=128.5;
+frame.apNum=2; frame.apSep=50;
+
+% should stay fixed across apertures
+frame.apSize=100; %number of pixels in aperture
+frame.backLum=128.5
+
+stim(1).amp= .5;
+stim(1).sf=3;
+stim(1).rad=3;
+stim(1,1).type='spiral'
+stim(1,1).rad=3;% remember aperture defined between +/=pi
+stim(1,1).ap='circular'; %or gaussian
+stim(1,1).npix=frame.apSize;
+
+
+stim(1,2).amp=1;stim(2).sf=2;
+stim(1,2).orient=90;stim(2).type='spokes';
+stim(1,2).rad=2; stim(2).ap='circular';
+stim(1,2).npix=frame.apSize;
+
+stim(2,1).amp=3; stim(2).sf=2;
+stim(2,1).orient=90;stim(2).type='radial';
+stim(2,1).rad=2; stim(2).ap='circular';
+stim(2,1).npix=frame.apSize;
+
+stim().amp=.5; stim(2).sf=2;
+stim(4).orient=-45;stim(2).type='grating';
+stim(4).rad=2; stim(2).ap='gaussian';
+stim(4).npix=frame.apSize;
+
+%% build the array
+bigMatSize = [(frame.apNum+1)*frame.apSep] + ...
+    [frame.apSize * frame.apNum];
+bigMat=frame.backLum * ones(bigMatSize);
+stpt = [frame.apSep (frame.apSep *2)+ frame.apSize];
+for i = 1:frame.apNum
+    for j=1:frame.apNum
+        [ sw2D ] = make2Dsinewave(stim(i,j)
+        [ sw2D ] = putinaperture(sw2D, stim((i,j)
