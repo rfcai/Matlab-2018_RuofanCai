2f396760ff2c4a4ed3f1e92feb4a547bf0fc75cd
Unknown <rfcai@uw.edu>
2018-02-18 20:43:58 -0800

    Homework6

--- Homework6_RuofanCai.m
+++ Homework6_RuofanCai.m
@@ -1,16 +1,18 @@
 %% 6.1
-m=100
-n=round(100/.618);
-[X,Y] = meshgrid(linspace(-1,1,m));
-X(30:40, 10:90)=.25
-X(60:70, 10:90)=.75
-imagesc(X);
-figure(1)
-colormap(gray(256))
-figure(2)
-colormap(autumn)
-figure(3)
-colormap(summer)
+[X,Y]=meshgrid(-pi:.01:pi);
+R=X.^2+Y.^2;
+
+R2=R<1.2.^2;
+R3=R<
+image(R2)
+cmap= [.25 .25 .25; 1 1 1];
+colormap(cmap)
+axis equal
+
+M=ones(size(X));
+N=1+M(:, round(1:size(M,2)/2));
+Q=[M N M];
+imagesc(Q)
 
 %% 6.2
 n=701
@@ -27,7 +29,6 @@ axis equal
 radiusimage=sqrt(X.^2+Y.^2);
 aperture=F;
 aperture(radiusimage<radius)=1;
-% aperture(radiusimage>=radius)=0;
 
 illusion=mod(atan2(Y,X)./pi*nseg,1).*aperture;
 imagesc(illusion);
